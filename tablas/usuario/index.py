d5bd742514031cb70189743ddc1ca9e770bb584a
Facundo-Romano <facuromano121299@gmail.com>
2024-08-21 17:33:03 -0300

    metodos usuario

---
+++ tablas/usuario/index.py
@@ -0,0 +1,20 @@
+def obtener_usuarios():
+    return 'usuarios'
+
+def obtener_usuario(id_usuario):
+    return 'usuario'
+
+def crear_usuario(usuario):
+    return 'success'
+
+def modificar_usuario(usuario):
+    return 'success'
+
+def asignar_rol_usuario(id_usuario, id_rol):
+    return 'success'
+
+def asignar_equipo_usuario(id_usuario, id_equipo):
+    return 'success'
+
+def eliminar_usuario(id_usuario):  
+    return 'success'
