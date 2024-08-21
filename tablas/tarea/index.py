7999a42efee955db78290e71aaff990abdc1b5ac
Facundo-Romano <facuromano121299@gmail.com>
2024-08-21 17:26:28 -0300

    metodos tarea

---
+++ tablas/tarea/index.py
@@ -0,0 +1,17 @@
+def obtener_tareas():
+    return 'tareas'
+
+def obtener_tarea(id_tarea):
+    return 'tarea'
+
+def crear_tarea(tarea):
+    return 'success'
+
+def modificar_tarea(tarea):
+    return 'success'
+
+def asignar_tarea(id_tarea, id_usuario):
+    return 'success'
+
+def eliminar_tarea(id_tarea):
+    return 'success'
