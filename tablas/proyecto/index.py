4d9d58f5d303092b8196527d2cbed9ea594ad448
Facundo-Romano <facuromano121299@gmail.com>
2024-08-21 17:19:18 -0300

    metodos proyecto

---
+++ tablas/proyecto/index.py
@@ -0,0 +1,14 @@
+def obtener_proyectos(id_equipo):
+    return 'proyectos'
+
+def obtener_proyecto(id_proyecto):
+    return 'proyecto'
+
+def crear_proyecto(proyecto):
+    return 'success'
+
+def modificar_proyecto(proyecto):
+    return 'success'
+
+def eliminar_proyecto(id_proyecto):
+    return 'success'
