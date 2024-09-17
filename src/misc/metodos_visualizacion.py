bd7b7018427e12ad22a40a497f16ff16557db660
wein11 <santiwei2805@gmail.com>
2024-09-17 19:21:22 -0300

    mostrar tareas

--- src/misc/metodos_visualizacion.py
+++ src/misc/metodos_visualizacion.py
@@ -1,7 +1,9 @@
 def mostrar_tareas(tareas):
     for tarea in tareas:
-        print()
+        print("Tareas")
         print(f"UUID: {tarea['uuid_prueba']}, Título: {tarea['titulo']}, Descripción: {tarea['descripcion']}, "
               f"UUID Usuario: {tarea['uuid_usuario']}, UUID Proyecto: {tarea['uuid_proyecto']}, "
               f"Fecha de Creación: {tarea['created_at']}, Fecha de Fin: {tarea['end_date']}")
+        print()  # Salto de línea para separar cada tarea
+
 visualizar_matriz = lambda matrix: print("\n".join([" | ".join([str(element) for element in row]) for row in matrix]))
