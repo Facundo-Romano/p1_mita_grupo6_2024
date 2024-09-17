238465b8df9a404d2e8e606221b4958fc4c8afe9
wein11 <santiwei2805@gmail.com>
2024-09-17 19:08:56 -0300

    mostrar tarea

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from tablas.tarea.metodos_tarea import crear_tarea, modificar_tarea, obtener_tareas, eliminar_tarea
+from misc.metodos_visualizacion import mostrar_tareas
 
 usuarios = [
     ['uuid', 'nombre', 'apellido', 'equipo', 'mail', 'contraseña', 'created_at'],
@@ -6,6 +7,7 @@ usuarios = [
 
 def menu_principal():
     while True:
+        print()
         print("Menú principal")
         print("1. Crear tarea")
         print("2. Modificar tarea")
@@ -22,10 +24,10 @@ def menu_principal():
             # Llamar a la función para modificar tarea
             modificar_tarea()
         elif opcion == "3":
+            print()
             # Llamar a la función para obtener tarea
             tareas = obtener_tareas()
-            for tarea in tareas:
-                print(tarea)
+            mostrar_tareas(tareas)
         elif opcion == "4":
             # Llamar a la función para eliminar tarea
             eliminar_tarea()
