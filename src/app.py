ba5bb28c424155b2c16597efa430eb5bbf804ed5
Facundo-Romano <facuromano121299@gmail.com>
2024-10-16 17:26:01 -0300

    progreso de login

--- src/app.py
+++ src/app.py
@@ -1,14 +1,14 @@
 from misc.metodos_visualizacion import limpiar_consola
-from modulos.login.modulo_login import login_menu
-from modulos.tareas.modulo_tareas import menu_tareas
+from modulos.login.modulo_login import menu_login
+""" from modulos.tareas.modulo_tareas import menu_tareas
 from modulos.proyectos.modulo_proyecto import menu_proyectos
-from modulos.equipos.modulo_equipos import menu_equipos
+from modulos.equipos.modulo_equipos import menu_equipos """
 
 def menu_principal():
     print('Hola')
 
     #Llamar a login
-    usuario = login_menu()
+    usuario = menu_login()
 
     limpiar_consola()
 
@@ -29,13 +29,13 @@ def menu_principal():
 
         if opcion == '1':
             #Llamar menu equipos
-            menu_equipos(usuario)
+            """ menu_equipos(usuario) """
         elif opcion == '2':
             #Llamar menu proyectos
-            menu_proyectos(usuario)
+            """ menu_proyectos(usuario) """
         elif opcion == '3':
             #Llamar menu tareas
-            menu_tareas(usuario)
+            """ menu_tareas(usuario) """
         elif opcion == '4':
             break
         else:
--- src/modulos/login/modulo_login.py
+++ src/modulos/login/modulo_login.py
@@ -1,5 +1,8 @@
-def login_menu():
-    print('login')
+from misc.metodos_visualizacion import limpiar_consola
+
+def menu_login():
+    limpiar_consola()
+    print('menu login')
 
     while True:
         print('1. Iniciar sesión')
@@ -8,11 +11,44 @@ def login_menu():
         opcion = input('Elija una opción: ')
 
         if opcion == '1':
-            print('Iniciar sesión')
+            login()
         elif opcion == '2':
-            print('Registrarse')
+            registrar()
         elif opcion == '3':
             break
         else:
             print('Opción no válida')
+
+
+def login():
+    try:
+        archivo_usuarios = open(r'd:/usuarios.txt', 'r', encoding='UTF-8')
+
+        mail = input('Ingrese su mail: ')
+
+        contraseña = input('Ingrese su contraseña: ')
+
+        linea = archivo_usuarios.readline()
+
+        while linea:
+            usuario = linea.strip().split(';')
+
+            if mail == usuario[3] and contraseña == usuario[4]:
+                return usuario
+            
+            linea = archivo_usuarios.readline()
+
+        raise Exception('Usuario no encontrado')
+
+ 
+    except Exception as e:
+        print(e)
+        return False
+
+    finally:
+        archivo_usuarios.close()
+    
+
+def registrar():
+    return
 
